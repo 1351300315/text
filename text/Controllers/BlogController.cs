be1aea2c09d7893595e48c41a790dde4925d63d9
stu <stu@qq.com>
2016-11-09 12:32:42 +0800

    认证登录

--- text/Controllers/BlogController.cs
+++ text/Controllers/BlogController.cs
@@ -31,8 +31,18 @@ namespace text.Controllers
         }
 
         public ActionResult AddArticle()
+
         {
-           return View();
+            if (Request.Cookies["isauth"] != null && Request.Cookies["isauth"].Value == "true")
+                           {
+                               return View();
+                           }
+                       else
+           {
+                                return RedirectToAction("login", "cookiedemo");
+                            }
+
+           
         }
 
         //public ActionResult ArticleSave(string subject, string body)
--- text/Controllers/CookieDemoController.cs
+++ text/Controllers/CookieDemoController.cs
@@ -22,11 +22,29 @@ namespace text.Controllers
             {
                 count = int.Parse(cookie.Value) + 1;
                 var newcookie = new HttpCookie("count", count.ToString());
-                newcookie.Expires = DateTime.Now.AddDays(7);
+                newcookie.Expires = DateTime.Now.AddSeconds(7);
                 Response.Cookies.Add(newcookie);
             }
 
             ViewBag.count = count;
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+
+
+
+            return View();
+        }
+        public ActionResult PostLogin(string username, string password)
+        {
+            if (username == "abc" && password == "123")
+            {
+                var cookie = new HttpCookie("isauth", "true");
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("AddArticle", "Blog");
+            }
             return View();
         }
     }
